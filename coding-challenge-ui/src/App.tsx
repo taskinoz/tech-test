627e140807b032f391fd3586bb9b50d2038d8616
Kirsty Purcell <kirstympurcell@gmail.com>
2021-12-08 11:25:23 +1100

    Update to typscript, update dates and update README

--- coding-challenge-ui/src/App.tsx
+++ coding-challenge-ui/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const AppWrapper = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: #cccccc;
-`
+`;
 
 const AppHeader = styled.header`
   background-color: white;
@@ -14,24 +14,31 @@ const AppHeader = styled.header`
   justify-content: space-between;
   align-items: center;
   padding: 0rem 2rem;
-`
+`;
 
 const HeaderText = styled.h1`
-  font-family: 'Roboto', sans-serif;
-`
+  font-family: "Roboto", sans-serif;
+`;
 
 const Username = styled.span`
-  font-family: 'Roboto', sans-serif;
-`
+  font-family: "Roboto", sans-serif;
+`;
+
+interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  id: number;
+}
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState<User | null>(null);
 
   React.useEffect(() => {
-    fetch('http://localhost:8080/user')
-      .then(results => results.json())
-      .then(data => {
-       setUser(data)
+    fetch("http://localhost:8080/user")
+      .then((results) => results.json())
+      .then((data) => {
+        setUser(data);
       });
   }, []);
 
@@ -39,11 +46,11 @@ const App = () => {
     <AppWrapper>
       <AppHeader>
         <HeaderText>Analytics Dashboard</HeaderText>
-        <Username>Welcome, {user ? user.firstName : 'Guest'}!</Username>
+        <Username>Welcome, {user ? user.firstName : "Guest"}!</Username>
       </AppHeader>
       {/** Dashboard - new widgets go here */}
     </AppWrapper>
   );
-}
+};
 
 export default App;
